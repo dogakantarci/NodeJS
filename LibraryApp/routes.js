1bfa92f94ff3119432585db08e8c9db56e5f544c
doa <dogakantarci@gmail.com>
2024-08-01 16:55:11 +0300

    Person A

--- LibraryApp/routes.js
+++ LibraryApp/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Book = require('./models/Book');
 
-
 // Ana sayfa rotası
 router.get('/', (req, res) => {
     res.send('Anasayfaya hoş geldiniz!');
