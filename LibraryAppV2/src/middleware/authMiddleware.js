a02e557046419b4a82556c687ad49086d0354108
doa <dogakantarci@gmail.com>
2024-09-26 17:57:39 +0300

    Fix: authMiddleware Error Handling

--- LibraryAppV2/src/middleware/authMiddleware.js
+++ LibraryAppV2/src/middleware/authMiddleware.js
@@ -2,8 +2,25 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');  // Kullanıcı modelini ekleyin
 
-exports.protect = async (req, res, next) => {
+// Hata mesajlarını düzenlemek için özel bir fonksiyon
+const handleTokenError = (error, res) => {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ message: 'Token has expired' });
+    } else if (error.name === 'JsonWebTokenError') {
+      return res.status(401).json({ message: 'Invalid token' });
+    } else {
+      return res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  };
+
+exports.authenticateUser = async (req, res, next) => {
     let token;
+
+      // Çevresel değişken kontrolü
+    if (!process.env.JWT_SECRET) {
+        return res.status(500).json({ message: "JWT secret is not defined." });
+  }
+
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
@@ -16,12 +33,20 @@ exports.protect = async (req, res, next) => {
             // Kullanıcıyı bulma
             req.user = await User.findById(decoded.id).select('-password');
 
+            // Kullanıcı bulunamazsa
+            if (!req.user) {
+                return res.status(404).json({ message: 'User not found' });
+      }
+
+
             next();
-        } catch (error) {
+    }       catch (error) {
+            // Token doğrulama hatalarını işle
             console.error(error);
-            return res.status(401).json({ message: 'Not authorized, token failed' });
-        }
-    } else {
-        return res.status(401).json({ message: 'Not authorized, no token' });
+            return handleTokenError(error, res);
     }
+  } else {
+    // Token yoksa
+    return res.status(401).json({ message: 'Not authorized, no token' });
+  }
 };
