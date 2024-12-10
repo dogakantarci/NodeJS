79e5f03e4a0b6119c0b8c32d92aed06d6a0e9273
doa <dogakantarci@gmail.com>
2024-12-10 04:25:50 +0300

    Feat: Add Docker and nginx

--- LibraryAppV2/src/redisClient.js
+++ LibraryAppV2/src/redisClient.js
@@ -1,11 +1,10 @@
-// src/redisClient.js
 const Redis = require('ioredis');
 
 // Redis'e bağlanmak için yeni bir Redis örneği oluşturun
 const redis = new Redis({
-  host: 'localhost',  // Redis sunucusunun adresi
-  port: 6379,         // Redis sunucusunun portu
-  db: 0               // Kullanmak istediğiniz veritabanı (varsayılan: 0)
+  host: 'my-redis',  // Docker Compose içindeki Redis servis adı
+  port: 6379,        // Redis sunucusunun portu
+  db: 0              // Kullanmak istediğiniz veritabanı (varsayılan: 0)
 });
 
 // Bağlantının başarılı olduğunu kontrol et
